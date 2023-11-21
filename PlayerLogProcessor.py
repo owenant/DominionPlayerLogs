7719025f9bc3b6fd6f3a373a46c1a43a07bf7610
owenant <anthonyowen1@gmail.com>
2023-11-21 20:50:48 +0000

    refactored code and split out main loop into individual functions

--- PlayerLogProcessor.py
+++ PlayerLogProcessor.py
@@ -19,6 +19,7 @@ def processor_log_file(input_dir, html_doc, output_dir):
     
     #first extract number of players and final score
     #look for entries of the form #n <name>: x points' - these are tagged with a 'b'
+    #in earlier log files the '#n' is dropped and hence we also need to consider this case
     players = []
     scores = []
     list_b_tags = soup.find_all('b');
@@ -30,13 +31,34 @@ def processor_log_file(input_dir, html_doc, output_dir):
             #next sibing contains the score, assume score is after a string of the form ': ' and is 2 digits long
             score_text = tag.next_sibling
             scores.append(score_text[2:4])
+
+    #deal with second case where name is not preceeded by a '#n '
+    if len(players) == 0:
+        for tag in list_b_tags:
+            s = tag.text
+            if ':' in s:
+                #in this case the name is contained in the text preceeded by a ':'
+                colon_position = s.index(':')
+                players.append(s[:colon_position])
+                scores.append(s[colon_position+1 : colon_position+3])
     
-    #extract number of turns for this game
-    total_turns = 0
+    #extract number of turns for this game for each player
+    total_turns = []
+    count = 0
     for (k,r) in enumerate(pre_tag.contents):
         if 'turn' in r.text:
-            total_turns = int(re.search(r'\d+', r.text).group())
-            break
+            turn_position = r.text.index('turn')
+            #assume number of turns is given by two digits and there is a space to the start of the word 'turn'
+            total_turns.append(int(re.search(r'\d+', r.text[turn_position-3:turn_position]).group()))
+            count += 1
+            if count == len(players):
+                break
+            
+    #note, turns are in same order as player names in html file
+    turns_by_player = {}
+    turns_by_player[players[0]] = total_turns[0]
+    turns_by_player[players[1]] = total_turns[1]
+    
     
     #following function parses a single line which consists of number of cards (unless equal to one) and card types
     #each separated by a colon and ended with a full stop or a horizontal dashed line. It returns a list of cards 
@@ -65,64 +87,49 @@ def processor_log_file(input_dir, html_doc, output_dir):
     #next extract kingdom cards - first we deal with the case where the default set is chosen and players don't 
     #make changes
     
-    #iterate through contents list under we find an element which contains 'cards in supply'. 
-    default_supply_cards_start_index = 0
-    for i, s in enumerate(pre_tag.contents):
-        if 'cards in supply' in s:
-            default_supply_cards_start_index = i
-            break
+    #so we need to be careful figuring out the cards in the supply, first we scan down the file
+    # to see if there is a 'chosen cards are' string followed by ''<player name> vetoes'. In this
+    #case the players are making choices over the card supply, if this isnt present then the
+    #supply cards are given at the top of the file post a string that says 'cards in supply'
+
+    #start with the case where players can veto
+    vetoes_used = False
+    for (i,s) in enumerate(pre_tag.contents):
+        if 'chosen cards are' in s:
+            #loop over chosen supply cards
+            (index, cards) = parse_row_into_cards(pre_tag.contents[i:])
+            index_end = i + index 
+            supply_cards = cards
+            vetoes_used = True
     
-    #there is an initial default set of supply cards. This may be modified later by player choice if this default 
-    #selection is not chosen. So first determine if the players use the default set
-    default_supply_set = False
-    for s in pre_tag.contents[default_supply_cards_start_index:]:
-        if 'Default card selection was used' in s:
-            default_supply_set = True
-            break
-        elif '----------------------' in s:
-            break
-           
-    #if the default set was used then grab card names
-    supply_cards = [] 
-    if default_supply_set == True:
-        (index, cards) = parse_row_into_cards(pre_tag.contents[default_supply_cards_start_index:])
-        supply_cards = cards
-        
-    #next we extract supply cards when players make a choice. In this case we look for the substring 'chosen cards are'
-    #which tags the start of the set of chosen supply cards. This should be followed by a number of veto statements
-    #dependent on the number of players
-    index_end = 0 
-    if default_supply_set == False:
-        for (i,s) in enumerate(pre_tag.contents[default_supply_cards_start_index:]):
-            if 'chosen cards are' in s:
-                #loop over chosen supply cards
-                (index, cards) = parse_row_into_cards(pre_tag.contents[default_supply_cards_start_index + i + 1:])
-                index_end = default_supply_cards_start_index + i + index + 1
+    if vetoes_used == True:
+        #next each player can veto a card
+        vetoed_cards = []
+        for player in players:
+            for s in pre_tag.contents[index_end:]:
+                check_string = player + ' vetoes'
+                if check_string in s:
+                    vetoed_cards.append(s.next_sibling.text)
+        #remove vetoed cards from list of supply cards
+        supply_cards = list(filter(lambda x: x not in vetoed_cards, supply_cards))
+            
+    #if this didnt occur move onto the second case
+    if vetoes_used == False:
+        for i, s in enumerate(pre_tag.contents):
+            if 'cards in supply' in s:
+                (index, cards) = parse_row_into_cards(pre_tag.contents[i:])
                 supply_cards = cards
-    
-    #next each player can veto a card
-    vetoed_cards = []
-    for player in players:
-        for s in pre_tag.contents[index_end:]:
-            check_string = player + ' vetoes'
-            if check_string in s:
-                vetoed_cards.append(s.next_sibling.text)
-    
-    #remove vetoed cards from list of supply cards
-    supply_cards = list(filter(lambda x: x not in vetoed_cards, supply_cards))
-    
-    #next loop through the turns and extract and bought or gained cards
-    
+
     #use a dictionary of dictionaries to track gained cards by turn by player, and initialise keys
     incremental_cards_by_turn = {}
     for player in players:
         incremental_cards_by_turn[player] = {}
-        for turn in range(1,total_turns+1):
+        for turn in range(1,turns_by_player[player]+1):
             incremental_cards_by_turn[player][turn] = {}
             incremental_cards_by_turn[player][turn] = {}
             incremental_cards_by_turn[player][turn] = {}
             incremental_cards_by_turn[player][turn] = {}
-        for turn in range(1,total_turns+1):
+        for turn in range(1,turns_by_player[player]+1):
             incremental_cards_by_turn[player][turn]['buys'] = {}
             incremental_cards_by_turn[player][turn]['trashing'] = {}
             incremental_cards_by_turn[player][turn]['gaining'] = {}
@@ -144,7 +151,7 @@ def processor_log_file(input_dir, html_doc, output_dir):
         check_player_gains = [ x + ' gains' for x in players]
         #string to check end of turn
         check_turn_end = '(' + player + ' draws:'
-        for (i,p) in enumerate(pre_tag.contents[index_end:]):
+        for (i,p) in enumerate(pre_tag.contents):
             check_turn = player + '\'s' + ' turn ' + str(turn_counter)
             if check_turn in p:
                 buy_card_list = []
@@ -152,39 +159,42 @@ def processor_log_file(input_dir, html_doc, output_dir):
                 gaining_card_list = []
                 gains_card_list = {} #needs to be a dictionary as multiple players may gain in another player's round
                 trashes_card_list = {} #needs to be a dictionary as multiple players may need to trash in another player's round
-                for (j,q) in enumerate(pre_tag.contents[index_end + i:]):
+                for player_ in players:
+                    gains_card_list[player_] = [] 
+                    trashes_card_list[player_] = [] 
+                for (j,q) in enumerate(pre_tag.contents[i:]):
                     player_gains_list = [ x in q for x in check_player_gains]
                     player_trashes_list = [ x in q for x in check_player_trashes]
                     if check_buy in q:
-                        (index, cards) = parse_row_into_cards(pre_tag.contents[index_end + i + j:])
-                        buy_card_list = cards
+                        (index, cards) = parse_row_into_cards(pre_tag.contents[i + j:])
+                        buy_card_list.append(cards)
                     elif check_trashing in q:
                         if 'trashing nothing' in r:
                             break
-                        (index, cards) = parse_row_into_cards(pre_tag.contents[index_end + i + j:])
-                        trashing_card_list = cards
+                        (index, cards) = parse_row_into_cards(pre_tag.contents[i + j:])
+                        trashing_card_list.append(cards)
                     elif check_gaining in q:
                         #also we need to check for a 'gaining nothing' case
                         if 'gaining nothing' in r:
                             break
                         else:
-                            (index, cards) = parse_row_into_cards(pre_tag.contents[index_end + i + j:])
-                            gaining_card_list = cards
+                            (index, cards) = parse_row_into_cards(pre_tag.contents[i + j:])
+                            gaining_card_list.append(cards)
                     elif any(player_gains_list):
                         #next we check if an opponent gains a card, e.g. a curse card
                         #need to loop through opponents
                         for (index, player_gains_check) in enumerate(player_gains_list):
                             if player_gains_check == True:
                                 player_ = players[index]
-                                (index_, cards) = parse_row_into_cards(pre_tag.contents[index_end + i + j:])
-                                gains_card_list[player_] = cards
+                                (index_, cards) = parse_row_into_cards(pre_tag.contents[i + j:])
+                                gains_card_list[player_].append(cards)
                     elif any(player_trashes_list):
                         #next we check if an opponent trashes a card
                         for (index, player_trashes_check) in enumerate(player_trashes_list):
                             if player_trashes_check == True:
                                 player_ = players[index]
-                                (index_, cards) = parse_row_into_cards(pre_tag.contents[index_end + i + j:])
-                                trashes_card_list[player_] = cards
+                                (index_, cards) = parse_row_into_cards(pre_tag.contents[i + j:])
+                                trashes_card_list[player_].append(cards)
                     elif check_turn_end in q.text:
                         incremental_cards_by_turn[player][turn_counter]['buys'] = buy_card_list
                         incremental_cards_by_turn[player][turn_counter]['trashing'] = trashing_card_list
@@ -195,7 +205,13 @@ def processor_log_file(input_dir, html_doc, output_dir):
                             incremental_cards_by_turn[player_][turn_counter]['trashes'] = trashes_card_list[player_] 
                         turn_counter += 1
                         break
-         
+    
+    #finally we flatten incremental_cards_by_turn into a dictionary of lists
+    for player in players:
+        for turns in range(1, turns_by_player[player]+1):
+            for cmd_type in incremental_cards_by_turn[player][turns].keys():
+                flattened_list = [item for sublist in incremental_cards_by_turn[player][turns][cmd_type] for item in sublist]
+                incremental_cards_by_turn[player][turns][cmd_type] = flattened_list
     
     #first categorise supply cards if possible
     supply_sets= {}
